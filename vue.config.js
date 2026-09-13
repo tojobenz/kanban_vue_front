f3061d4e48a6f52e40117514306326813b33ec3b
Aina Tojosoa <ainatojosoa@gmail.com>
2026-09-13 06:58:26 +0200

    fix: config node

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   devServer: {
     port: 8081
+  },
+  configureWebpack: {
+    optimization: {
+      minimize: false
+    }
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Kanban Board';
+      return args;
+    });
   }
 }
